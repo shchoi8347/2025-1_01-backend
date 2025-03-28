321e166abc7941a05a74d2a522ae6c63568458a4
shchoi8347 <csh@duksung.ac.kr>
2025-03-28 11:35:42 +0900

    수업완료코드: 01반 20250327(목)

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -1,12 +1,17 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.TodoDTO;
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -49,7 +54,43 @@ public class TodoService {
     public List<TodoEntity> retrieve(final String userId) {
         return repository.findByUserId(userId);
     }
+
+    public List<TodoEntity> update(TodoEntity entity) {
+        validate(entity);
+
+        final Optional<TodoEntity> original = repository.findById(entity.getId());
+
+        original.ifPresent(todo -> {
+            todo.setTitle(entity.getTitle());
+            todo.setDone(entity.isDone());
+
+            repository.save(todo);
+        });
+
+        return retrieve(entity.getUserId());
+    }
+
+    public List<TodoEntity> delete(final TodoEntity entity) {
+        validate(entity);
+
+        try {
+            repository.delete(entity);
+        } catch (Exception e) {
+            log.error("error deleting entity", entity.getId(), e);
+            throw new RuntimeException("error deleting entity" + entity.getId());
+        }
+
+        return retrieve(entity.getUserId());
+    }
+
+
 }
+
+
+
+
+
+
 
 
 
